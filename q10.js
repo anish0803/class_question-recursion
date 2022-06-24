f6bfc36747241165566dd5bbad52188f722b50df
anish0803 <singhanish124@gmail.com>
2022-06-24 09:24:16 +0530

    commit even again

--- q10.js
+++ q10.js
@@ -1,17 +1,47 @@
-function Even(a, b){
-     if (b < a){
-    
-return ;
-    }
-     if (b % 2 == 0 ) {
+
+
+
+
+
+
+
+// Given a number n, print all the even numbers from 1 to n using recursion.
+
+
+
+function even (a,b){
    
-    Even(a, b - 2);
-    }else {
-     Even(a, b - 1);
-    }
-     if (b % 2 == 0) {
-   
-       console.log(b);
-    }
+  if (a<=b) {
+      if (a%2!==1){
+      console.log(a);
+      return even (a+1,b);
+  }
+  return even (a+1,b);
 }
-Even(1,20);
+}
+
+even(1,10);
+
+
+
+
+
+
+
+// function Even(a, b){
+//      if (b < a){
+    
+// return ;
+//     }
+//      if (b % 2 == 0 ) {
+   
+//     Even(a, b - 2);
+//     }else {
+//      Even(a, b - 1);
+//     }
+//      if (b % 2 == 0) {
+   
+//        console.log(b);
+//     }
+// }
+// Even(1,20);
